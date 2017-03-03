6933b5ab9f7f267e083500a462ddd6a97ad15a96
Cameron Batt <vespertilian@gmail.com>
2017-03-03 11:59:48 +0000

    moving to mocha reporter for nicer colours

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,6 @@ module.exports = function(config) {
     var settings = {
         basePath: '',
         frameworks: ['jasmine'],
-
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
@@ -23,23 +21,13 @@ module.exports = function(config) {
         },
         webpackMiddleware: { noInfo: true },
 
-        specReporter: {
-            maxLogLines: 5, // limit number of lines logged per test
-            suppressErrorSummary: true, // do not print error summary
-            suppressFailed: false, // do not print information about failed tests
-            suppressPassed: false, // do not print information about passed tests
-            suppressSkipped: true, // do not print information about skipped tests
-            showSpecTiming: false // print the time elapsed for each spec
-        },
+        reporters: ['mocha'],
+        browsers: ['Chrome'],
+        singleRun: false,
+        concurrency:  Infinity,
         mime: {
             'text/x-typescript': ['ts','tsx']
         }
     };
-
-    settings.reporters = ['spec']; // Add spec reporter for local testing
-    settings.browsers = ['Chrome'];
-    settings.singleRun = false;
-    settings.concurrency = Infinity;
-
     config.set(settings);
 };
